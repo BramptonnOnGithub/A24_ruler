3a8eb6f44d02cc4bacf9d52ca92398af33cb134d
Bramptonn <ramtin.bakhshi05@gmail.com>
2024-12-26 19:02:09 -0500

    First

---
+++ PowerCouple/PowerCoupleRuler/PowerCoupleRuler/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * PowerCoupleRuler.cpp
+ *
+ * Created: 2024-12-26 6:46:15 PM
+ * Author : ramti
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
