fbd539cc79eb47423e905a513cefc1cf4014044b
Bramptonn <ramtin.bakhshi05@gmail.com>
2024-12-27 00:41:16 -0500

    encoder

---
+++ PowerCouple/PowerCoupleRuler/PowerCoupleRuler/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Buttons.cpp
+
+Display.cpp
+
+Encoder.cpp
+
+main.cpp
+
+SD_Card.cpp
+
+UART.cpp
+
--- PowerCouple/PowerCoupleRuler/PowerCoupleRuler/Encoder.cpp
+++ PowerCouple/PowerCoupleRuler/PowerCoupleRuler/Encoder.cpp
@@ -3,9 +3,15 @@
  *  Author: faryc
  */ 
 #include "Encoder.h"
+#define RADIUS 13.5
+#define PI 3.14
+#define RESOLUTION 24
 
 volatile int encoderPosition = 0;
 volatile int lastState = 0;
+float circumference = (2*PI*RADIUS) / RESOLUTION;
+float distance;
+int ticks;
 
 bool encoderInit(){
 	//Configure pins as input
@@ -18,7 +24,32 @@ bool encoderInit(){
 	PCICR |= (1 << PCIE0); 
 	PCMSK0 |= (1 << PCINT0) | (1 << PCINT1); //Enables it for PB0 and PB1
 	
+	//PE3 always high
+	DDRE |= (1 << PE3);
+	PORTE |= (1 << PE3);
+	
+	//PB2 always low
+	DDRB |= (1 << PB2);
+	PORTB &= ~(1 << PB2);
+	
 	//Setup initial state of pins
 	lastState = PINB & ((1 << PB0) | (1 << PB1));
+	
+}
+
+float encodergetPosition(){
+	return circumference;
+	
+}
+
+int encoderReset(){
+	encoderPosition = 0
+	return 1;
+	
+}
+
+float encoderCalc(){
+	distance = circumference * ticks;
+	return distance;
 	
 }
